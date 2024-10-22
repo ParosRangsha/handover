53b0fcc355e0343dac76fe6fd67b3dde0aa7236c
paros <paroscrangsha@gmail.com>
2024-10-23 02:03:42 +0600

    new setup

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-let productIdToFind = String(112077);
-let filteredDispos = dispos.filter(d => d.DS === productIdToFind);
-let list = document.querySelector("ul")
-list.style.width= "350px"
-for(i=0;i<filteredDispos.length;i++){
-    list.innerHTML += `<li><a href="#">${filteredDispos[i].DS}</a> <a href="#">${filteredDispos[i].SR}</a> <a href="#">${filteredDispos[i].ER}</a>  <a href="#">${filteredDispos[i].TR}</a> <a href="#"> ${filteredDispos[i].QT}</a> <a href="#">${filteredDispos[i].RM}</a> </li>`
-}
-let li= document.querySelector("li")
-li.style.width= "100%"
-li.style.display= "flex"
-li.style.justifyContent= "space-between "
+
+let btnCheck = document.querySelector("#btnCheck")
+let list = document.querySelector("ol")
+
+
+
+
+
+
+
+
+
+
+btnCheck.addEventListener("click",()=>{
+    list.innerHTML = ""
+    let inpTxt = document.querySelector("#inpDispo")
+    let productIdToFind = Number(inpTxt.value);
+    let filteredDispos = dispos.filter(d => d.DS === productIdToFind);
+    
+    for(i=0;i<filteredDispos.length;i++){
+        list.innerHTML += `<li><a href="#">${filteredDispos[i].DS}</a> <a href="#">${filteredDispos[i].SR}</a> <a href="#">${filteredDispos[i].ER}</a>  <a href="#">${filteredDispos[i].TR}</a> <a href="#"> ${filteredDispos[i].QT}</a> <a href="#">${filteredDispos[i].RM}</a> <a href="#">${filteredDispos[i].DT}</a> </li>`
+    }
+})
