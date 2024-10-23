cbc83103e619cae5427163df4f55c1f712340157
paros <paroscrangsha@gmail.com>
2024-10-23 08:30:39 +0600

    excel button change

--- js/script.js
+++ js/script.js
@@ -1,16 +1,6 @@
 
 let btnCheck = document.querySelector("#btnCheck")
 let list = document.querySelector("ol")
-
-
-
-
-
-
-
-
-
-
 btnCheck.addEventListener("click",()=>{
     list.innerHTML = ""
     let inpTxt = document.querySelector("#inpDispo")
@@ -20,8 +10,12 @@ btnCheck.addEventListener("click",()=>{
     for(i=0;i<filteredDispos.length;i++){
         list.innerHTML += `<li><span>${filteredDispos[i].DS}</span><span>${filteredDispos[i].SR}</span><span>${filteredDispos[i].ER}</span><span>${filteredDispos[i].TR}</span><span>${filteredDispos[i].QT}</span><span>${filteredDispos[i].RM}</span><span>${filteredDispos[i].DT}</span></li>`
     }
+})
+let clickDownload = document.querySelector("#fileExcel")
+clickDownload.addEventListener("click",()=>{
+    let inpTxtDownload = document.querySelector("#inpDispo")
     let excelDownload = document.querySelector("#excel")
-    if(inpTxt.value == "excelDownload"){
+    if(inpTxtDownload.value == "excelDownload"){
         excelDownload.style.display= "inline-block"
     }else{
         excelDownload.style.display= "none"
