4e0ae5c499036636d697cac8affb669f9a704aef
paros <paroscrangsha@gmail.com>
2024-10-23 08:19:14 +0600

    excel add kora hoyeche

--- js/script.js
+++ js/script.js
@@ -20,4 +20,10 @@ btnCheck.addEventListener("click",()=>{
     for(i=0;i<filteredDispos.length;i++){
         list.innerHTML += `<li><span>${filteredDispos[i].DS}</span><span>${filteredDispos[i].SR}</span><span>${filteredDispos[i].ER}</span><span>${filteredDispos[i].TR}</span><span>${filteredDispos[i].QT}</span><span>${filteredDispos[i].RM}</span><span>${filteredDispos[i].DT}</span></li>`
     }
+    let excelDownload = document.querySelector("#excel")
+    if(inpTxt.value == "excelDownload"){
+        excelDownload.style.display= "inline-block"
+    }else{
+        excelDownload.style.display= "none"
+    }
 })
